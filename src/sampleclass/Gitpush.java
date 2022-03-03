95d68f63e131914c5d67d944f7421931f6475929
akashbelkhedkarap <100796323+akashbelkhedkarap@users.noreply.github.com>
2022-03-03 22:19:15 +0530

    First commit to git.

---
+++ src/sampleclass/Gitpush.java
@@ -0,0 +1,11 @@
+package sampleclass;
+
+public class Gitpush {
+	public static void main(String[] args) {
+		System.out.println("this is my first git class");
+	
+	
+	    System.out.println("this is my second Updation");
+	}
+
+}
